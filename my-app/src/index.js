a1a2bb98bc0f7365c7430319d723368f3a584d02
ladynskaya95 <feverrr27@gmail.com>
2021-11-13 20:33:08 +0200

    second commit

---
+++ my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.css"
+import './index.css';
+import App from './components/app/';
+//import reportWebVitals from './reportWebVitals';
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root')
+);
+
+//reportWebVitals();
